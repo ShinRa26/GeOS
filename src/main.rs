117e197576bd63191f9b8a064292ec939908be91
Graham Keenan <grahamkeenan@hotmail.co.uk>
2018-11-29 11:08:06 +0000

    Autocommit

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,7 @@ fn panic(_info: &PanicInfo) -> ! {
 static HELLO: &[u8] = b"Hello world!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
-        }
-    }
+    println!("Hello world{}", "!");
 
     loop{}
 }
